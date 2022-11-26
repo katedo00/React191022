31b03104a24f43989ded1802484b86d66c511d6f
katedo00 <meodenmeohen@gmail.com>
2022-11-26 09:24:46 +0700

    homework editable table

---
+++ JS_EventLoop/l1.js
@@ -0,0 +1,23 @@
+import fetch from "node-fetch";
+function getData() {
+  fetch("https://jsonplaceholder.typicode.com/users", {
+  method: "GET",
+  headers: {
+
+}
+}).then(response => response.json().then(res => {return res})) }
+
+async function syncData() {
+  let promise = new Promise((resolve) => {
+      resolve(fetch("https://jsonplaceholder.typicode.com/users", {
+        method: "GET",
+        headers: {
+      
+      }
+      }).then(response => response.json().then(res => {return res})));
+  });
+  let data = await promise; 
+  console.log(data);
+  return data;
+};
+syncData()
